592ae92594fdbf41f7069b6bfc4a178b2d0cd00d
Nick Schoess <schoessnick@gmail.com>
2020-06-28 16:43:02 -0500

    added code for burgers_controller.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    let output = {
+      burger: data
+    };
+    res.render("index", output);
+  });
+});
+
+router.post("/api/add-burger", function (req, res) {
+  burger.insertOne([req.body.name], function (data) {
+  });
+});
+
+router.put("/api/eat-burger", function (req, res) {
+  burger.updateOne([req.body.id], function (data) {
+  });
+});
+
+module.exports = router;
