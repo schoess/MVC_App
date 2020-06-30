412986140b66003bfe8561969d64fe1140122732
Nick Schoess <schoessnick@gmail.com>
2020-06-30 15:51:17 -0500

    added basic handlebars views

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,22 @@
 const orm = require("../config/orm.js");
+
+
+let burger = {
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
+    });
+  },
+  insertOne: function (newName, cb) {
+    orm.insertOne("burgers", "burger_name", newName, function (res) {
+      cb(res);
+    });
+  },
+  updateOne: function (id, cb) {
+    orm.updateOne("burgers", "devoured", true, id, function (res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
